03fd3971a8ba1f39b8df2c7dad3e7d1c13f8aa59
Daniel Vargas <daniel.vargas@maihiro.com>
2020-05-20 21:19:26 -0500

    File Reader

---
+++ src/main/java/com/s4n/io/FileReader.java
@@ -0,0 +1,20 @@
+package com.s4n.io;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class FileReader {
+
+
+	public List<String> readFile(final String path) throws IOException {
+		Stream<String> lines = Files.lines(Paths.get(path), StandardCharsets.UTF_8);
+		List<String> result = lines.collect(Collectors.toList());
+		return result;
+	}
+}
